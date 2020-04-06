01b0b109b9884874e782aafa4edf70fad33ca9ae
Daniel García <nelo686@gmail.com>
2020-04-06 02:20:09 +0200

    Commit: - Build detail view

--- app/src/main/java/es/mrmoustard/tmdb/ui/common/Extensions.kt
+++ app/src/main/java/es/mrmoustard/tmdb/ui/common/Extensions.kt
@@ -1,5 +1,6 @@
 package es.mrmoustard.tmdb.ui.common
 
+import android.text.SpannableString
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,4 +15,21 @@ fun FragmentActivity.showMessage(view: View, style: SnackbarStyle) {
     val snackbar = Snackbar.make(view, style.message, Snackbar.LENGTH_SHORT)
     snackbar.setBackgroundTint(style.getBackgroundColor(context = this))
     snackbar.show()
+}
+
+fun SpannableString.spanWith(target: String, apply: SpannableBuilder.() -> Unit) {
+    val builder = SpannableBuilder()
+    apply(builder)
+
+    val start = this.indexOf(target)
+    val end = start + target.length
+
+    for (what in builder.what) {
+        setSpan(what, start, end, builder.flags)
+    }
+}
+
+class SpannableBuilder {
+    lateinit var what: List<Any>
+    var flags: Int = 0
 }
--- app/src/main/java/es/mrmoustard/tmdb/ui/detail/DetailActivity.kt
+++ app/src/main/java/es/mrmoustard/tmdb/ui/detail/DetailActivity.kt
@@ -2,14 +2,29 @@ package es.mrmoustard.tmdb.ui.detail
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.AbsoluteSizeSpan
+import android.text.style.StyleSpan
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.text.toSpanned
 import androidx.lifecycle.Observer
+import coil.api.load
 import es.mrmoustard.tmdb.R
 import es.mrmoustard.tmdb.app
 import es.mrmoustard.tmdb.di.detail.DetailModule
-import es.mrmoustard.tmdb.ui.detail.DetailUiModel.Loading
+import es.mrmoustard.tmdb.domain.entities.MovieDetail
+import es.mrmoustard.tmdb.ui.common.ErrorSnackbarStyle
+import es.mrmoustard.tmdb.ui.common.showMessage
+import es.mrmoustard.tmdb.ui.common.spanWith
+import es.mrmoustard.tmdb.ui.detail.DetailUiModel.*
+import kotlinx.android.synthetic.main.activity_movie_details.*
+import kotlinx.android.synthetic.main.view_header.view.*
+import kotlinx.android.synthetic.main.view_highlight_header.view.*
 import kotlinx.android.synthetic.main.view_progress.*
 import javax.inject.Inject
 
@@ -33,14 +48,84 @@ class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie_details)
         component.inject(activity = this)
 
         viewModel.onViewCreated(intent.extras?.getInt(MOVIE_ID) ?: -1)
         viewModel.model.observe(this, Observer(::updateUi))
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        ivFavourite.setOnClickListener { }
+        ivWannaWatch.setOnClickListener { }
     }
 
     private fun updateUi(model: DetailUiModel) {
         loader.visibility = if (model is Loading) View.VISIBLE else View.GONE
+
+        when (model) {
+            is Content -> setContent(model.movie)
+            is ErrorResponse -> showMessage(
+                view = clRoot,
+                style = ErrorSnackbarStyle(message = getString(R.string.something_happen))
+            )
+        }
     }
+
+    private fun setContent(movie: MovieDetail) {
+        ivPoster.load("https://image.tmdb.org/t/p/w780${movie.backdropPath}")
+        tvTitle.text = movie.title
+
+        setTagline(tagline = movie.tagline)
+
+        with(highlight) {
+            tvAverage.text = SpannableString("${movie.voteAverage}/10").apply {
+                spanWith(movie.voteAverage.toString()) {
+                    what = listOf(AbsoluteSizeSpan(21, true), StyleSpan(Typeface.BOLD))
+                    flags = Spanned.SPAN_EXCLUSIVE_INCLUSIVE
+                }
+            }.toSpanned()
+
+            tvVoteCount.text = "${movie.voteCount} votes"
+            tvReleaseDate.text = "${tvReleaseDate.text}: ${movie.releaseDate}"
+            tvGenres.text = "${tvGenres.text}: ${movie.genres.map { it.name }.readList()}"
+            tvOriginalTitle.text = "${tvOriginalTitle.text}: ${movie.originalTitle}"
+            tvOriginalLanguage.text = "${tvOriginalLanguage.text}: ${movie.originalLanguage}"
+            tvSpokenLanguages.text =
+                "${tvSpokenLanguages.text}: ${movie.spokenLanguages.map { it.name }.readList()}"
+        }
+
+        overviewHeader.tvHeaderTitle.text = getString(R.string.overview_header)
+        tvOverview.text = movie.overview
+    }
+
+    private fun setTagline(tagline: String) {
+        if (tagline.isNotEmpty()) {
+            tvTagLine.text = "\"${tagline}\""
+        } else {
+            ConstraintSet().apply {
+                clone(clRoot)
+                connect(
+                    R.id.highlight, ConstraintSet.TOP,
+                    R.id.tvTitle, ConstraintSet.BOTTOM
+                )
+                applyTo(clRoot)
+            }
+            tvTagLine.visibility = View.GONE
+        }
+    }
+
+    private fun List<String>.readList(): String =
+        when (size) {
+            0 -> getString(R.string.not_available)
+            1 -> first()
+            else -> {
+                var result: String = first()
+                for (i in 1 until size) {
+                    result += ", ${get(i)}"
+                }
+                result
+            }
+        }
 }
